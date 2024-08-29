ec550ae66ebdb8433661d8f004882755cc1aa2a8
Firdig Alfalakhi <Alfalakhie@gmail.com>
2024-08-29 13:39:16 +0700

    fix cron route

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,8 +1,7 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import { generateRecipe } from "@/lib/recipe-lib/generate-recipe";
 import { NextResponse } from "next/server";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: Request) {
   await generateRecipe();
   return new NextResponse("Recipe generation triggered", { status: 200 });
 }
